ada30e398d3c5ec09b920b601dc5d5f5fe60feca
Mohmed Shawky <57459035+mohamedshawky70@users.noreply.github.com>
2025-02-26 22:43:02 +0200

    Update UserController.cs

--- UserController.cs
+++ UserController.cs
@@ -1,8 +1,18 @@
 [HttpGet("")]
+//Pagination , Searching and Sorting
 public async Task<IActionResult> GetAllAsync([FromQuery]FilterRequest filter, CancellationToken cancellationToken)
 {
-	var users = await _userManager.Users.ToListAsync(cancellationToken);
+	//Searching
+	var users =  _userManager.Users.AsQueryable();
+	if (!string.IsNullOrEmpty(filter.SearchValue))
+		users = users.Where(u => u.UserName!.Contains(filter.SearchValue));
+	//Sorting
+	//To know write the expression in OrderBy(--) install package System.Linq.Dynamic.Core and write using System.Linq.Dynamic.Core;
+	if (!string.IsNullOrEmpty(filter.SortColumn))
+		users = users.OrderBy($"{filter.SortColumn} {filter.SortDirection}");
+		
 	var response = users.Adapt<IEnumerable<UserResponse>>();
+	//Pagination
 	var userList = await PaginationList<UserResponse>.CreateAsync(response, filter.PageNumber, filter.PageSize);
 	return Ok(userList);
 }
